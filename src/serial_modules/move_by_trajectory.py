fb1cfeaf9f55e02a822636fd4476ea371b2ca85a
Soluthy <soluthyplus@gmail.com>
2021-07-12 21:44:46 -0300

    feat: grande avanço na comunicação

--- src/serial_modules/move_by_trajectory.py
+++ src/serial_modules/move_by_trajectory.py
@@ -3,7 +3,7 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from src.serial_modules import move_by_point
 
 
-def move_by_trajectory(path: str, client: Type[ModbusClient], user_reg: int):
+def move_by_trajectory(path: str, client: Type[ModbusClient], user_id: int):
     """Mover por trajetória
 
     :param path: caminho do arquivo da trajetória
@@ -12,17 +12,15 @@ def move_by_trajectory(path: str, client: Type[ModbusClient], user_reg: int):
     """
 
     with open(path, "r") as file:
-
         for linha in file:
             pontos = linha.split()
-            x_axis = pontos[0]
-            y_axis = pontos[1]
+            x_axis = int(pontos[0])
+            y_axis = int(pontos[1])
             move_by_point(
                 x_axis=x_axis,
                 y_axis=y_axis,
-                user_reg=user_reg,
+                user_id=user_id,
                 trajectory=path,
                 client=client,
             )
-
         file.close()
